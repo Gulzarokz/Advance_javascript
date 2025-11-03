3e402fc2ed039ba72c6c2906df447a7f1a3ad8f0
Gulzarokz <gokz22301@gmail.com>
2025-11-03 16:11:06 +0500

    done let var const

---
+++ index.js
@@ -0,0 +1,35 @@
+// This is the main JavaScript file for the Advance 🙌 javascript project
+//var, let and const
+//two versions of javascript
+//ES5 - 2009
+//ES6 old var, let - 2015 new version const and let
+
+// we will used both version of es6 and es5
+// es5 - var
+// es6 - let and const
+// difference between var, let and const
+// 1. var is function scoped
+// var add self into window object
+// function testVar() {
+//     for (var i = 0; i < 5; i++) {
+//         console.log(i);
+//     }
+//     console.log(i);
+
+// }
+// testVar();
+//    let are  block scoped
+// let con't add self into window object
+// function testLet() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//     }
+//     console.log(i);
+
+// }
+// testLet();
+// 2. var can be re-declared and updated
+//    let can be updated but not re-declared
+//    const can neither be updated nor re-declared
+//
+
